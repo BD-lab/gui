7d4c250efe9ef00efdfc653bdb5ac325b3b5d5b1
pogodagreen <pogodagreen@gmail.com>
2019-12-14 09:36:58 +0100

    changed from qt4 to qt5

--- ChoosePatientWindow.py
+++ ChoosePatientWindow.py
@@ -2,67 +2,55 @@
 
 # Form implementation generated from reading ui file 'ChoosePatientWindow.ui'
 #
-# Created by: PyQt4 UI code generator 4.12.1
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_ChoosePatientWindow(object):
     def setupUi(self, ChoosePatientWindow):
-        ChoosePatientWindow.setObjectName(_fromUtf8("ChoosePatientWindow"))
+        ChoosePatientWindow.setObjectName("ChoosePatientWindow")
         ChoosePatientWindow.resize(800, 600)
-        self.centralwidget = QtGui.QWidget(ChoosePatientWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.formLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.centralwidget = QtWidgets.QWidget(ChoosePatientWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.formLayoutWidget.setGeometry(QtCore.QRect(200, 150, 431, 221))
-        self.formLayoutWidget.setObjectName(_fromUtf8("formLayoutWidget"))
-        self.formLayout = QtGui.QFormLayout(self.formLayoutWidget)
-        self.formLayout.setMargin(0)
-        self.formLayout.setObjectName(_fromUtf8("formLayout"))
-        self.pesel = QtGui.QLabel(self.formLayoutWidget)
-        self.pesel.setObjectName(_fromUtf8("pesel"))
-        self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.pesel)
-        self.peselInput = QtGui.QLineEdit(self.formLayoutWidget)
-        self.peselInput.setObjectName(_fromUtf8("peselInput"))
-        self.formLayout.setWidget(0, QtGui.QFormLayout.FieldRole, self.peselInput)
-        self.chooseButton = QtGui.QPushButton(self.formLayoutWidget)
-        self.chooseButton.setObjectName(_fromUtf8("chooseButton"))
-        self.formLayout.setWidget(1, QtGui.QFormLayout.FieldRole, self.chooseButton)
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.pesel = QtWidgets.QLabel(self.formLayoutWidget)
+        self.pesel.setObjectName("pesel")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.pesel)
+        self.peselInput = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.peselInput.setObjectName("peselInput")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.peselInput)
+        self.chooseButton = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.chooseButton.setObjectName("chooseButton")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.chooseButton)
         ChoosePatientWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtGui.QStatusBar(ChoosePatientWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar = QtWidgets.QStatusBar(ChoosePatientWindow)
+        self.statusbar.setObjectName("statusbar")
         ChoosePatientWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(ChoosePatientWindow)
         QtCore.QMetaObject.connectSlotsByName(ChoosePatientWindow)
 
     def retranslateUi(self, ChoosePatientWindow):
-        ChoosePatientWindow.setWindowTitle(_translate("ChoosePatientWindow", "MainWindow", None))
-        self.pesel.setText(_translate("ChoosePatientWindow", "Pesel", None))
-        self.chooseButton.setText(_translate("ChoosePatientWindow", "Wybierz", None))
+        _translate = QtCore.QCoreApplication.translate
+        ChoosePatientWindow.setWindowTitle(_translate("ChoosePatientWindow", "MainWindow"))
+        self.pesel.setText(_translate("ChoosePatientWindow", "Pesel"))
+        self.chooseButton.setText(_translate("ChoosePatientWindow", "Wybierz"))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    ChoosePatientWindow = QtGui.QMainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    ChoosePatientWindow = QtWidgets.QMainWindow()
     ui = Ui_ChoosePatientWindow()
     ui.setupUi(ChoosePatientWindow)
     ChoosePatientWindow.show()
     sys.exit(app.exec_())
-
--- gui.py
+++ gui.py
@@ -2,58 +2,43 @@
 
 # Form implementation generated from reading ui file 'gui.ui'
 #
-# Created by: PyQt4 UI code generator 4.12.1
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_Main(object):
     def setupUi(self, Main):
-        Main.setObjectName(_fromUtf8("Main"))
+        Main.setObjectName("Main")
         Main.resize(800, 600)
-        self.centralwidget = QtGui.QWidget(Main)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.ChoosePatient = QtGui.QPushButton(self.centralwidget)
+        self.centralwidget = QtWidgets.QWidget(Main)
+        self.centralwidget.setObjectName("centralwidget")
+        self.ChoosePatient = QtWidgets.QPushButton(self.centralwidget)
         self.ChoosePatient.setGeometry(QtCore.QRect(314, 186, 131, 61))
-        self.ChoosePatient.setObjectName(_fromUtf8("ChoosePatient"))
-        self.AddPatient = QtGui.QPushButton(self.centralwidget)
+        self.ChoosePatient.setObjectName("ChoosePatient")
+        self.AddPatient = QtWidgets.QPushButton(self.centralwidget)
         self.AddPatient.setGeometry(QtCore.QRect(314, 266, 131, 61))
-        self.AddPatient.setObjectName(_fromUtf8("AddPatient"))
+        self.AddPatient.setObjectName("AddPatient")
         Main.setCentralWidget(self.centralwidget)
-        self.statusbar = QtGui.QStatusBar(Main)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        Main.setStatusBar(self.statusbar)
 
         self.retranslateUi(Main)
         QtCore.QMetaObject.connectSlotsByName(Main)
 
     def retranslateUi(self, Main):
-        Main.setWindowTitle(_translate("Main", "MainWindow", None))
-        self.ChoosePatient.setText(_translate("Main", "Wybierz Pacjenta", None))
-        self.AddPatient.setText(_translate("Main", "Dodaj Pacjenta", None))
+        _translate = QtCore.QCoreApplication.translate
+        Main.setWindowTitle(_translate("Main", "MainWindow"))
+        self.ChoosePatient.setText(_translate("Main", "Wybierz Pacjenta"))
+        self.AddPatient.setText(_translate("Main", "Dodaj Pacjenta"))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    Main = QtGui.QMainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    Main = QtWidgets.QMainWindow()
     ui = Ui_Main()
     ui.setupUi(Main)
     Main.show()
     sys.exit(app.exec_())
-
